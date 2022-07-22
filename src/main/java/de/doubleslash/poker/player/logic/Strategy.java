6fafb2c21b751d0dbad140cce34300c35889339a
Fabian Kock <Fabian.Kock@partner.bmwgroup.com>
2022-07-22 20:55:09 +0200

    win

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -1,5 +1,6 @@
 package de.doubleslash.poker.player.logic;
 
+import de.doubleslash.poker.player.data.Rank;
 import de.doubleslash.poker.player.data.Table;
 
 public class Strategy {
@@ -9,6 +10,10 @@ public class Strategy {
 
       if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0).equals(table.getPlayers().get(table.getActivePlayer()).getCards().get(1)))
       {
+         if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.QUEEN) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.JACK) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.KING) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.ACE))
+         {
+            return table.getMinimumBet() * 3;
+         }
          return table.getMinimumBet()*2;
       }
       return table.getMinimumBet();
