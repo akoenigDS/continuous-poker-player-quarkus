c708b61002add42678db9978f8082fbd42ebff0d
Fabian Kock <Fabian.Kock@partner.bmwgroup.com>
2022-07-22 21:17:19 +0200

    tripleCheckWin

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -1,5 +1,13 @@
 package de.doubleslash.poker.player.logic;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.doubleslash.poker.player.data.Card;
 import de.doubleslash.poker.player.data.Rank;
 import de.doubleslash.poker.player.data.Table;
 
@@ -23,6 +31,29 @@ public class Strategy {
       return table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.QUEEN) || table.getPlayers().get(
               table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.JACK) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.KING) || table.getPlayers().get(
               table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.ACE);
+   }
+
+   private boolean checkForTriple(final Table table) {
+      boolean result = false;
+
+      final Map<Card, Integer> cards = new HashMap<>();
+
+
+      for(final Card ownCard : table.getPlayers().get(table.getActivePlayer()).getCards()) {
+         cards.merge(ownCard, 1, Integer::sum);
+      }
+      for(final Card communityCard : table.getCommunityCards()) {
+         cards.merge(communityCard, 1, Integer::sum);
+      }
+
+      for(var entry : cards.entrySet())
+      {
+         if (entry.getValue() == 3){
+            return true;
+         }
+      }
+
+      return result;
    }
 
 }
