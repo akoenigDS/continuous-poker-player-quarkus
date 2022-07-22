28377779883ad3ec2a28a66c6268e93561ab2da9
Fabian Kock <Fabian.Kock@partner.bmwgroup.com>
2022-07-22 21:23:15 +0200

    formatting

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -1,11 +1,7 @@
 package de.doubleslash.poker.player.logic;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import de.doubleslash.poker.player.data.Card;
 import de.doubleslash.poker.player.data.Rank;
@@ -13,54 +9,50 @@ import de.doubleslash.poker.player.data.Table;
 
 public class Strategy {
 
-   public int decide(final Table table) {
-      System.out.println(table);
+    public int decide(final Table table) {
 
-      if (table.getCommunityCards().size() >= 3)
-      {
-         if(checkForTriple(table)) {
-            return table.getMinimumBet() * 4;
-         }
-      }
+        if (table.getCommunityCards().size() >= 3) {
+            if (checkForTriple(table)) {
+                return table.getMinimumBet() * 4;
+            }
+        }
 
-      if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0).equals(table.getPlayers().get(table.getActivePlayer()).getCards().get(1)))
-      {
-         if (isHigherThanJack(table))
-         {
-            return table.getMinimumBet() * 3;
-         }
-         return table.getMinimumBet()*2;
-      }
-      return table.getMinimumBet();
-   }
+        if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0)
+                .equals(table.getPlayers().get(table.getActivePlayer()).getCards().get(1))) {
+            if (isHigherThanJack(table)) {
+                return table.getMinimumBet() * 3;
+            }
+            return table.getMinimumBet() * 2;
+        }
+        return table.getMinimumBet();
+    }
 
-   private boolean isHigherThanJack(final Table table) {
-      return table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.QUEEN) || table.getPlayers().get(
-              table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.JACK) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.KING) || table.getPlayers().get(
-              table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.ACE);
-   }
+    private boolean isHigherThanJack(final Table table) {
+        return table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.QUEEN)
+                || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.JACK)
+                || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.KING)
+                || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.ACE);
+    }
 
-   private boolean checkForTriple(final Table table) {
-      boolean result = false;
+    private boolean checkForTriple(final Table table) {
+        boolean result = false;
 
-      final Map<Card, Integer> cards = new HashMap<>();
+        final Map<Card, Integer> cards = new HashMap<>();
 
+        for (final Card ownCard : table.getPlayers().get(table.getActivePlayer()).getCards()) {
+            cards.merge(ownCard, 1, Integer::sum);
+        }
+        for (final Card communityCard : table.getCommunityCards()) {
+            cards.merge(communityCard, 1, Integer::sum);
+        }
 
-      for(final Card ownCard : table.getPlayers().get(table.getActivePlayer()).getCards()) {
-         cards.merge(ownCard, 1, Integer::sum);
-      }
-      for(final Card communityCard : table.getCommunityCards()) {
-         cards.merge(communityCard, 1, Integer::sum);
-      }
+        for (var entry : cards.entrySet()) {
+            if (entry.getValue() == 3) {
+                return true;
+            }
+        }
 
-      for(var entry : cards.entrySet())
-      {
-         if (entry.getValue() == 3){
-            return true;
-         }
-      }
-
-      return result;
-   }
+        return result;
+    }
 
 }
