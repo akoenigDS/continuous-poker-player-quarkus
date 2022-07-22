afbde1ec73510012ef754dc984ac31146542d99f
Fabian Kock <Fabian.Kock@partner.bmwgroup.com>
2022-07-22 20:59:07 +0200

    refactor

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -10,13 +10,19 @@ public class Strategy {
 
       if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0).equals(table.getPlayers().get(table.getActivePlayer()).getCards().get(1)))
       {
-         if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.QUEEN) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.JACK) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.KING) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.ACE))
+         if (isHigherThanJack(table))
          {
             return table.getMinimumBet() * 3;
          }
          return table.getMinimumBet()*2;
       }
       return table.getMinimumBet();
+   }
+
+   private boolean isHigherThanJack(final Table table) {
+      return table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.QUEEN) || table.getPlayers().get(
+              table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.JACK) || table.getPlayers().get(table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.KING) || table.getPlayers().get(
+              table.getActivePlayer()).getCards().get(0).getRank().equals(Rank.ACE);
    }
 
 }
