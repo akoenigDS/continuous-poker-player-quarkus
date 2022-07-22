39420bd2cf2ca81a43940d4f61a36ccf5d25d087
Fabian Kock <Fabian.Kock@partner.bmwgroup.com>
2022-07-22 20:49:17 +0200

    paerchen raise

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -6,6 +6,11 @@ public class Strategy {
 
    public int decide(final Table table) {
       System.out.println(table);
+
+      if (table.getPlayers().get(table.getActivePlayer()).getCards().get(0).equals(table.getPlayers().get(table.getActivePlayer()).getCards().get(1)))
+      {
+         return table.getMinimumBet()*2;
+      }
       return table.getMinimumBet();
    }
 
